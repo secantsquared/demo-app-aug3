44d190fac1d6ac064c93f249286ae96922b208d2
Ryan Boris <ryan.boris@gmail.com>
2019-08-03 17:10:41 -0400

    :construction: Begin Specials comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [numbers, setNumbers] = useState([])
+  const [operators, setOperators] = useState([])
+  const [specials, setSpecials] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://my-json-server.typicode.com/ryanboris/demo-app-aug3/db')
+      .then(response => {
+        setNumbers(response.data.numbers)
+        setOperators(response.data.operators)
+        setSpecials(response.data.specials)
+      })
+      .catch(err => console.error(err))
+  }, [])
+
+  console.log('numbers', numbers)
+  console.log('operators', operators)
+  console.log('specials', specials)
   return <div className="App">App is rendering!</div>
 }
 
---
+++ src/Specials.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Specials = () => {
+  return <div />
+}
+
+export default Specials
